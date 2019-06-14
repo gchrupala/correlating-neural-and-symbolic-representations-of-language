d183f2c3259cb02147a43b822319db2460692bb8
Grzegorz Chrupała <g.chrupala@uvt.nl>
2019-06-14 17:56:25 +0200

    Cleanup kernel.py

--- rsa/kernel.py
+++ rsa/kernel.py
@@ -2,48 +2,12 @@ from functools import reduce
 import rsa.arithmetic as A
 import logging
 
-def match(n1, n2):
-    return (n1.is_leaf() and n2.is_leaf() and n1.value == n2.value) \
-                or \
-           (not n1.is_leaf() and not n2.is_leaf() and n1.op == n2.op)
-
-
-def C(n1, n2, alpha=1):
-    # both leaves
-    if n1.is_leaf() and n2.is_leaf() and match(n1, n2):
-        return alpha
-    # productions same and internal
-    elif match(n1, n2) and match(n1.left, n2.left) and match(n1.right, n2.right):
-        return alpha * (1 + C(n1.left, n2.left, alpha=alpha)) * (1 + C(n1.right, n2.right, alpha=alpha))
-    else:
-        return 0
-            
-
-def K(T1, T2, alpha=1.0):
-    N = sum(C(n1, n2, alpha=alpha) for n1 in T1.nodes() for n2 in T2.nodes())
-    return N
 
 def deleaf(t):
     if t.is_leaf():
         return A.Expression(None)
     else:
         return A.Expression(t.op, left=deleaf(t.left), right=deleaf(t.right))
-
-def pairwise_old(T, normalize=False, ignore_leaves=False, device='cpu', alpha=1.0):
-    import torch
-    if ignore_leaves:
-        T = [ deleaf(t) for t in T ]
-    M = torch.zeros(len(T), len(T), dtype=torch.float32, device=device)
-    for i in range(len(T)):
-        for j in range(len(T)):
-            if i <= j:
-                M[i,j] = K(T[i], T[j], alpha=alpha)
-    if normalize:
-        diag = M.diag().unsqueeze(dim=0)
-        denom = (diag * diag.t())**0.5
-        return M/denom
-    else:
-        return M
 
 def pairwise(T1, T2=None, normalize=False, ignore_leaves=False, device='cpu', alpha=1.0):
     import torch
@@ -59,28 +23,6 @@ def pairwise(T1, T2=None, normalize=False, ignore_leaves=False, device='cpu', al
     K = Kernel(alpha=alpha)
     return torch.tensor(K.pairwise(T1, T2, normalize=normalize, dtype=np.float64))
 
-# def etree_(e):
-#     "What we claim in the paper."
-#     from nltk.tree import Tree
-#     if e.is_leaf(): 
-#          return Tree('E', [str(e.value)]) 
-#     else: 
-#          return Tree('E', ['(', etree(e.left), e.op, etree(e.right), ')'])
-# def etree_(e):
-#     "What we claim in the paper with a twist."
-#     from nltk.tree import Tree
-#     if e.is_leaf(): 
-#          return Tree('E', [str(e.value)]) 
-#     else: 
-#          return Tree(e.op, ['(', etree(e.left), e.op, etree(e.right), ')'])
-def _etree(e):
-    "This one is the obvious rep."
-    from nltk.tree import Tree
-    if e.is_leaf(): 
-         return Tree('D', [str(e.value)]) 
-    else: 
-         return Tree('E', [Tree('L', ['(']), etree(e.left), Tree('O', [e.op]), etree(e.right), Tree('R', [')'])])
-
 def etree(e):
     "This one is the obvious rep."
     from nltk.tree import Tree
@@ -88,14 +30,6 @@ def etree(e):
          return Tree('E', [Tree('D', [str(e.value)])])
     else: 
          return Tree('E', [Tree('L', ['(']), etree(e.left), Tree('O', [e.op]), etree(e.right), Tree('R', [')'])])
-    
-# def etree_(e):
-#     "This one is very close to original results"
-#     from nltk.tree import Tree 
-#     if e.is_leaf():  
-#         return Tree('D', [str(e.value)]) 
-#     else:  
-#         return Tree(e.op, [etree(e.left), etree(e.right)])
     
 def cosine(A): 
      M = A @ A.t() 
